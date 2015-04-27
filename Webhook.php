de336484aac6d88a9ffefbfc568c149251616b81
Ruben te Bogt <info@webrtb.com>
2015-04-27 15:26:17 +0200

    Versie 0.2

--- Webhook.php
+++ Webhook.php
@@ -7,16 +7,27 @@ $LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
 $REMOTE_REPO        = "git@github.com:WebRTB/Webhook.git";
 $BRANCH             = "develop";
 
-if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push')
+if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'ping')
 {
 	if( file_exists($LOCAL_REPO) )
 	{
-		shell_exec("cd {$LOCAL_REPO} && git pull origin {$BRANCH}");
-		die("done " . mktime());
+		die("Ping done " . time());
+	}
+}
+elseif ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push')
+{
+	if( file_exists($LOCAL_REPO) )
+	{
+		shell_exec("cd {$LOCAL_REPO} && git pull");
+		die("Push done " . time());
 	}
 	else
 	{
 		shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
-		die("done " . mktime());
+		die("Clone done " . time());
 	}
+}
+else
+{
+	die("Not a valid Github event " . time());
 }
